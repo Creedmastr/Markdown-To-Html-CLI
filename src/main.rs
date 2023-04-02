bf4c3c326b202f98976090906adbabfc2a12f436
Creedmastr <thegaminglife09@gmail.com>
2023-04-02 09:37:00 +0200

    before gpt

--- src/main.rs
+++ src/main.rs
@@ -2,30 +2,33 @@ mod get_content;
 
 fn find_word_line(word: &str, lines: &Vec<String>) -> (String, u32) {
     let mut index: u32 = 0;
-    let mut result: String = "".to_string(); 
+    let mut result: String = "".to_string();
     for item in lines {
-        index += 1;
         if item.contains(word) {
             result.push_str(item);
             break;
         }
-    };
+        index += 1;
+    }
 
     (result, index)
 }
 
 fn main() {
     let content = get_content::get_content();
-    let words = content.0;
-    let mut lines = content.1;
+    let words = content.0.clone();
+    let mut lines = content.1.clone();
 
     let mut result = String::new();
-    
+    let mut to_remove: String = String::new();
+
     for item in words {
         let item_word_line = find_word_line(&item, &lines);
 
         match item {
             x if x == String::from("#") => {
+                to_remove.push_str(&item_word_line.0.as_str().as_ref());
+
                 result.push_str("<h1> ");
                 result.push_str(&item_word_line.0.as_str().replace("# ", "").as_ref());
                 result.push_str(" </h1> ");
@@ -33,7 +36,9 @@ fn main() {
 
             _ => {
                 result.push_str(&item);
-                lines.remove(item_word_line.1 as usize);
+                if item_word_line.1 > 0 && item_word_line.1 < lines.len().try_into().unwrap() {
+                    to_remove.push_str(&item);
+                }
             }
         }
     }
