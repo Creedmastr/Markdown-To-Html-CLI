fdb5a6520983e2c161b0840f835ae48b206c127e
Creedmastr <thegaminglife09@gmail.com>
2023-04-09 18:27:14 +0200

    added a font

--- src/html.rs
+++ src/html.rs
@@ -7,6 +7,12 @@ pub fn to_valid_html(previous_result: String) -> String {
             <meta charset='utf-8'>
             <meta http-equiv='X-UA-Compatible' content='IE=edge'>
             <title>Page Title</title>
+            <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto+Mono'>
+            <style>
+                body {
+                font-family: 'Roboto Mono', monospace;
+                }
+            </style>
             <meta name='viewport' content='width=device-width, initial-scale=1'>
         </head>
         <body> \n
